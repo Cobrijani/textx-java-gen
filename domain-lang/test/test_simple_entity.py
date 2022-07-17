d5b826d8c82c133a1838643f42a0b210266c03e7
Cobrijani <stefanbraticns@gmail.com>
2022-07-17 20:05:45 +0200

    Added data types

--- domain-lang/test/test_simple_entity.py
+++ domain-lang/test/test_simple_entity.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from distutils.log import debug
 import os
 import pytest
 from textx import (metamodel_for_language,
@@ -10,16 +11,21 @@ current_dir = os.path.dirname(__file__)
 def clear_all():
     clear_language_registrations()
     
+
+def __retrieve_model(test_file):
+    """Retrieve model for given language
+    """
+    mmF = metamodel_for_language('domain_lang')
+    return mmF.model_from_file(os.path.join(current_dir,
+                                             'models',
+                                             test_file), debug=True)
     
     
 def test_simple_entities(clear_all):
     """
     Testing simple entities
     """
-    mmF = metamodel_for_language('domain_lang')
-    model = mmF.model_from_file(os.path.join(current_dir,
-                                             'models',
-                                             'simple_entities.mdl'))
+    model = __retrieve_model("simple_entities.mdl")
     assert(model is not None)
     assert(len(model.entities) == 2)
     
@@ -27,9 +33,31 @@ def test_cardinality(clear_all):
     """
     Testing cardinality entities
     """
-    mmF = metamodel_for_language('domain_lang')
-    model = mmF.model_from_file(os.path.join(current_dir,
-                                             'models',
-                                             'entity_cardinality.mdl'), debug=False)
+    model = __retrieve_model("entity_cardinality.mdl")
     assert(model is not None)
     assert(len(model.entities) == 6)
+    
+
+def test_enum(clear_all):
+    """Testing enums
+
+    Args:
+        clear_all (_type_): _description_
+    """
+    model = __retrieve_model("simple_enums.mdl")
+    assert(model is not None)
+    assert(len(model.entities) == 1)
+    assert(len(model.enums) == 1)
+
+def test_data_types(clear_all):
+    """Test complex type in a language
+
+    Args:
+        clear_all (_type_): _description_
+    """
+    model = __retrieve_model("data_type.mdl")
+
+    assert(model is not None)
+    assert(len(model.data_types) == 2)
+    assert(len(model.enums) == 1)
+    
